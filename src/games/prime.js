905b259084e5c37bdd2187719273809e33f62846
Pleshakov Alexsnder <0542969@mail.ru>
2024-05-03 21:00:38 +0300

    create brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import game from '../index.js';
+
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    } return true;
+  }
+};
+
+const getRandomInt = (minimum, maximum) => {
+  const min = Math.ceil(minimum);
+  const max = Math.floor(maximum);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const getQuestionsAndAnswer = () => {
+  const question = getRandomInt(1, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
+
+export default () => {
+  game(rules, getQuestionsAndAnswer);
+};
