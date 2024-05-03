87f1b7b30f157db34759b65c849de5f0dc1de8a5
Pleshakov Alexsnder <0542969@mail.ru>
2024-05-03 21:22:42 +0300

    fix eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -13,4 +13,24 @@ const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: plug
 export default [
   {languageOptions: { globals: globals.browser }},
   ...compat.extends("airbnb-base"),
-];
+{
+  rules: {
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'import/extensions': [
+        'error',
+        {
+          js: 'always',
+        },
+      ],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
+  },
+]
