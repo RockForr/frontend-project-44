bf8b67e3c15ea9fe648cb0dd27dc85de4d5f153b
Pleshakov Alexsnder <0542969@mail.ru>
2024-05-05 18:17:06 +0300

    fix calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,27 +4,31 @@ import getRandomInt from '../utility.js';
 const rules = 'What is the result of the expression?';
 const operators = ['+', '-', '*'];
 
+const calculation = (num1, randomOperator, num2) => {
+  let result;
+
+  switch (randomOperator) {
+    case '+':
+      result = num1 + num2;
+      break;
+
+    case '-':
+      result = num1 - num2;
+      break;
+
+    default:
+      result = num1 * num2;
+  }
+  return result;
+};
+
 const getQuestionsAndAnswer = () => {
   const num1 = getRandomInt();
   const num2 = getRandomInt(1, 20);
   const randomOperator = operators[getRandomInt(0, 3)];
 
-  let expression;
-  switch (randomOperator) {
-    case '+':
-      expression = num1 + num2;
-      break;
-
-    case '-':
-      expression = num1 - num2;
-      break;
-
-    default:
-      expression = num1 * num2;
-  }
-
   const question = `${num1} ${randomOperator} ${num2}`;
-  const correctAnswer = expression.toString();
+  const correctAnswer = calculation(num1, randomOperator, num2).toString();
 
   return [question, correctAnswer];
 };
--- src/utility.js
+++ src/utility.js
@@ -1,4 +1,3 @@
-
 export default (minimum = 1, maximum = 100) => {
   const min = Math.ceil(minimum);
   const max = Math.floor(maximum);
